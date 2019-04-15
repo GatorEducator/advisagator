0db9004d6757e5a6ad7a60755c34466c3b986696
Spencer Huang <huangs@allegheny.edu>
2019-04-15 17:08:08 -0400

    Fix some pylint errors

--- advisagator/advising/apps.py
+++ advisagator/advising/apps.py
@@ -1,5 +1,8 @@
+""" This file helps the user to include any application configuration for the app.
+    and allows the user to configure some of the attributes of the application. """
 from django.apps import AppConfig
 
 
 class AdvisingConfig(AppConfig):
+    """This configs the name of the app"""
     name = "advising"
--- advisagator/advising/tests.py
+++ advisagator/advising/tests.py
@@ -1,3 +1,5 @@
+"""This is where you test the django methods"""
+#pylint: disable=W0611
 from django.test import TestCase
 
 # Create your tests here.
