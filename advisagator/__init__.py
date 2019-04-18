7d12f9fddc6be1fa2602c930074cad6da865b97d
Spencer Huang <huangs@allegheny.edu>
2019-04-18 13:47:42 -0400

    Fix flake8 and pylint errors

--- advisagator/__init__.py
+++ advisagator/__init__.py
@@ -1,12 +1,11 @@
 """ Application package initializer """
 import flask
-
-# main flask instance
-app = flask.Flask(__name__)
-
-
 from . import index
 from . import db_connect
 from . import students
 from . import teachers
 from . import login
+
+
+# main flask instance
+app = flask.Flask(__name__)
