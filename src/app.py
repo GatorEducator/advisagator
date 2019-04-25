7cc486f1cb63f0a24e44e72b977fbf1c36904495
Spencer Huang <huangs@allegheny.edu>
2019-04-24 20:25:41 -0400

    Add back all the lost files

--- src/app.py
+++ src/app.py
@@ -1,19 +1,7 @@
-""" This is undocumented """
+from flask import render_template, Flask, Response, redirect, url_for, request, abort, session
 import os
-
-# pylint: disable=unused-import
-from flask import (
-    render_template,
-    Flask,
-    Response,
-    redirect,
-    url_for,
-    request,
-    abort,
-    session,
-)
-
 import login_handler
+import database_handler
 
 
 app = Flask(__name__)
@@ -22,40 +10,33 @@ app.secret_key = os.urandom(16)
 
 @app.route("/", methods=["GET"])
 def home_redirect():
-    """ This is undocumented """
     return redirect("/home")
 
 
 @app.route("/home", methods=["GET"])
 def home_get():
-    """ This is undocumented """
-    # pylint: disable=no-else-return
-    if not session.get("logged_in"):
-        return render_template("home.html")
+    if not session.get('logged_in'):
+        return render_template('home.html')
     else:
         return redirect("/petitions")
 
 
 @app.route("/login", methods=["GET"])
 def login_get():
-    """ This is undocumented """
-    # pylint: disable=no-else-return
-    if not session.get("logged_in"):
-        return render_template("login.html")
+    if not session.get('logged_in'):
+        return render_template('login.html')
     else:
         return redirect("/petitions")
 
 
 @app.route("/login", methods=["POST"])
 def login_post():
-    """ This is undocumented """
-    email = request.form["email"]
-    password = request.form["password"]
+    email = request.form['email']
+    password = request.form['password']
     valid = login_handler.validate_user(email, password)
-    # pylint: disable=no-else-return
     if valid:
-        session["logged_in"] = True
-        session["email"] = email
+        session['logged_in'] = True
+        session['email'] = email
         return redirect("/petitions")
     else:
         return redirect("/invalid_login")
@@ -63,32 +44,82 @@ def login_post():
 
 @app.route("/invalid_login", methods=["GET"])
 def invalid_login_get():
-    """ This is undocumented """
     return render_template("invalid_login.html")
 
 
 @app.route("/logout", methods=["GET"])
 def logout_get():
-    """ This is undocumented """
     session.clear()
     return redirect("/home")
 
 
+@app.route("/change_password", methods=["GET"])
+def change_password_get():
+    if session.get('logged_in'):
+        return render_template("change_password.html")
+    else:
+        return redirect("/home")
+
+
+@app.route("/change_password", methods=["POST"])
+def change_password_post():
+    if session.get('logged_in'):
+        password = request.form['password']
+        confirm_password = request.form["confirm_password"]
+        if password == confirm_password:
+            # database_handler.update_password(session.get('email'), new_password)
+            print(password)
+            return redirect("/petitions")
+        else:
+            return redirect("/invalid_confirmation")
+    else:
+        return redirect("/home")
+
+
+@app.route("/invalid_confirmation", methods=["GET"])
+def invalid_confirmation_get():
+    if session.get('logged_in'):
+        return render_template("invalid_confirmation.html")
+    else:
+        return redirect("/home")
+
+
 @app.route("/petitions", methods=["GET"])
 def petitions_get():
-    """ This is undocumented """
-    # pylint: disable=no-else-return
-    if session.get("logged_in"):
-        return render_template("petitions.html")
+    if session.get('logged_in'):
+        petitions = database_handler.get_petitions(session['email'])
+        out_petitions = list()
+        for petition in petitions:
+            new_petition = {'id':petition[3],'name':petition[0],'email':petition[1],'department':petition[2]}
+            out_petitions.append(new_petition)
+        return render_template("petitions.html", petitions=out_petitions)
+    else:
+        return redirect("/home")
+
+
+@app.route("/petitions/<id>", methods=["GET"])
+def petitions_inspect_get(id):
+    if session.get('logged_in'):
+        petition = database_handler.get_petition_info(id)
+        new_petition_info = {'id':petition[4],'name':petition[0],'email':petition[1],'department':petition[3],'content':petition[2]}
+        return render_template("petition_info.html", petition_info=new_petition_info)
+    else:
+        return redirect("/home")
+
+
+@app.route("/petitions/<id>", methods=["POST"])
+def petitions_inspect_post(id):
+    if session.get('logged_in'):
+        approved = request.form['approved']
+        print(approved)
+        return redirect("/petitions")
     else:
         return redirect("/home")
 
 
 @app.errorhandler(404)
-# pylint: disable=unused-argument
 def page_not_found(e):
-    """ This is undocumented """
-    return render_template("404.html"), 404
+    return render_template('404.html'), 404
 
 
 if __name__ == "__main__":
