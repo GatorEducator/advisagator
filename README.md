48731789c31af2eed83af833933e072daa925c1d
Mohammad Khan <mohammadnk1@gmail.com>
2019-05-03 13:12:51 -0400

    Delete unneeded files

--- README.md
+++
@@ -1,8 +0,0 @@
-# adivisagator
-
-## How to use
-
-export FLASK_ENV=development
-`pipenv install --dev`
-*While in src folder*
-`pipenv run flask run`
--- tests/conf_test.py
+++
@@ -1,7 +0,0 @@
-""" Configure python to include parent directory modules """
-
-import os
-import sys
-
-PATH = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, PATH + "/../")
